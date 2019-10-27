39673f1b018e45f166110aecaafad60cc0088fae
pengqi-bc <dingyizhenbitcoin@gmail.com>
2019-10-27 13:00:19 +0800

    improve binary

--- barkis.sh
+++ barkis.sh
@@ -14,22 +14,17 @@ username=$(jq -r .username $configFile)
 ip=$(jq -r .ip $configFile)
 barkisPath=$(jq -r .barkisdPath $configFile)
 cert=$(jq -r .certPath $configFile)
-moniker=$(jq -r .moniker $configFile)
-genesis=$(jq -r .genesis $configFile)
-seed=$(jq -r .seed $configFile)
-persistent_peers=$(jq -r .persistent_peers $configFile)
+networkConfig=$(jq -r .networkConfig $configFile)
 gasPrice=$(jq -r .gasPrice $configFile)
 
 case "$command" in
   deploy)
-	scp -i $cert $barkisPath $username@$ip:~
 	scp -i $cert script/envCheck.sh $username@$ip:~
-	scp -i $cert $configFile $username@$ip:~/config.json
 	scp -i $cert script/deploy.sh $username@$ip:~
 	scp -i $cert script/start.sh $username@$ip:~
 
 	ssh -i $cert -t $username@$ip "bash envCheck.sh"
-	ssh -i $cert -t $username@$ip "bash deploy.sh config.json"
+	ssh -i $cert -t $username@$ip "bash deploy.sh $networkConfig"
 	ssh -i $cert -t $username@$ip "nohup bash start.sh $gasPrice"
 	;;
   start)
--- script/deploy.sh
+++ script/deploy.sh
@@ -5,15 +5,24 @@ barkiscli=$HOME/barkiscli
 
 home=$HOME/.barkisd
 
-configFile=$1
+configUrl=$1
+
+wget $configUrl/binary/barkisd
+wget $configUrl/binary/barkiscli
+wget $configUrl/genesis.json
+wget $configUrl/networkConfig.json
+
+chmod +x barkisd
+chmod +x barkiscli
+
+configFile=networkConfig.json
+
 moniker=$(jq -r .moniker $configFile)
-genesis=$(jq -r .genesis $configFile)
 seed=$(jq -r .seed $configFile)
 persistent_peers=$(jq -r .persistent_peers $configFile)
 
 $barkisd init $moniker
 
-wget $genesis
 mv genesis.json .barkisd/config/genesis.json
 
 sed -i -e "s/seeds = \"\"/seeds = \"$seed\"/g" $home/config/config.toml
